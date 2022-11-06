279ba5f3eda20e7aacb5ee18319e328f5c6efa8c
Nazar Voitekh <nazar.voitekh@gmail.com>
2022-11-07 00:41:43 +0200

    test

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const getSumCategories = document.querySelectorAll('#categories .item');
+const message = 'Number of categories:';
+console.log(message, getSumCategories.length);
+const getSumElements = document.querySelectorAll(' li');
+console.log(getSumElements);
